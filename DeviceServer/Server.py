e9b3e91ed0020e61a87323ec38b499ef1449d7bf
Randolph Voorhies <voorhies@usc.edu>
2011-11-09 23:53:20 -0800

    Adds connection logging

--- DeviceServer/Server.py
+++ DeviceServer/Server.py
@@ -2,13 +2,17 @@
 
 from twisted.internet import protocol, reactor
 from twisted.internet.protocol import Factory
+from twisted.python import log
 
 class DeviceServer(protocol.Protocol):
+  def connectionMade(self):
+    log.msg('New Connection: ' + str(self.transport.getHost()))
+
   def dataReceived(self, data):
-    print data
     self.transport.write(data)
     self.transport.loseConnection()
 
+log.startLogging(open('/var/log/ceres.log', 'w'))
 factory = Factory()
 factory.protocol = DeviceServer
 reactor.listenTCP(10000, factory)
