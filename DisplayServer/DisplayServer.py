72d3996aa829ce1612d1028f235891a801cf9f12
Randolph Voorhies <voorhies@usc.edu>
2011-11-14 23:51:14 -0800

    DisplayServer can now display data from any device id

--- DisplayServer/DisplayServer.py
+++ DisplayServer/DisplayServer.py
@@ -1,21 +1,41 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask, jsonify, render_template, request
+import pymongo
+
 app = Flask(__name__)
 
 @app.route('/_getdata')
 def getdata():
-  hdid = request.args.get('hdid')
-  return jsonify(result="You said: " + hdid)
+  global ceresdb
+  hwid = request.args.get('hwid')
+
+  result = ''
+  for entry in ceresdb.dataentries.find({'hwid' : hwid}).sort('time',1):
+    result += str(entry['time']) + ' - ' + str(entry['data']) + '<br>'
+  return jsonify(result=result)
 
 @app.route('/')
-@app.route('/<hdid>')
-def index(hdid=None):
-  if hdid == None:
+@app.route('/<hwid>')
+def index(hwid=None):
+  if hwid == None:
     return 'Give me a device id!'
   else:
-    return render_template('index.html', hdid=hdid)
+    deviceinfo = ceresdb.devices.find_one({'hwid' : hwid})
+    if deviceinfo == None:
+      alldevices = "<h1>All Devices:</h1>"
+      for device in ceresdb.devices.find():
+        alldevices += 'Device: ' + device['hwid'] + '<br>'
+      return alldevices
 
+    else:
+      return render_template('index.html', hwid=hwid)
+
+ceresdb = None
 if __name__ == '__main__':
+  global ceresdb
+
+  dbconnection = pymongo.Connection()
+  ceresdb = dbconnection.ceres
   app.run(debug=True)
 
