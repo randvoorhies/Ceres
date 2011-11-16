6356af5d023b85508ee8c8e166fb43c85d2bf39a
Randolph Voorhies <voorhies@usc.edu>
2011-11-15 21:16:01 -0800

    Displaying dates and values now

--- DisplayServer/DisplayServer.py
+++ DisplayServer/DisplayServer.py
@@ -1,36 +1,44 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, jsonify, render_template, request, json
 import pymongo
 
+ceresdb = None
 app = Flask(__name__)
 
 @app.route('/_getdata')
 def getdata():
   global ceresdb
   hwid = request.args.get('hwid')
+  print 'Getting data'
 
-  result = ''
+  result = []
   for entry in ceresdb.dataentries.find({'hwid' : hwid}).sort('time',1):
-    result += str(entry['time']) + ' - ' + str(entry['data']) + '<br>'
-  return jsonify(result=result)
+    result.append((str(entry['time']), str(entry['data'])))
+  return jsonify(data=result)
 
 @app.route('/')
-@app.route('/<hwid>')
+@app.route('/mydevice/<hwid>')
 def index(hwid=None):
+  global ceresdb
   deviceinfo = ceresdb.devices.find_one({'hwid' : hwid})
   if deviceinfo == None:
-    alldevices = "<h1>All Devices:</h1>"
+    # If no valid HWID was given in the URL, just display them all
+    alldevices = ''
+
+    if hwid != None:
+      alldevices += "<h1>Bad Device!</h1>"
+
+    alldevices += "<h2>All Devices:</h2>"
     for device in ceresdb.devices.find():
       alldevices += 'Device: ' + device['hwid'] + '<br>'
     return alldevices
 
   else:
+    # If a valid HWID was given, then render the data display template
     return render_template('index.html', hwid=hwid)
 
-ceresdb = None
 if __name__ == '__main__':
-  global ceresdb
 
   dbconnection = pymongo.Connection()
   ceresdb = dbconnection.ceres
